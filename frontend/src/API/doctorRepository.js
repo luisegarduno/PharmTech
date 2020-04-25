580bd91ce988e4b890b19517a1246a6460a358f7
Adam <aczarnik@smu.edu>
2020-04-24 19:23:10 -0700

    added new API calls

--- frontend/src/API/doctorRepository.js
+++ frontend/src/API/doctorRepository.js
@@ -21,6 +21,17 @@ export class DoctorRepository {
         });
     }
 
+    getDrugTypes() {
+        return new Promise((resolve, reject) => {
+            axios.get(`${this.url}/getDrugTypes`)
+                .then(x => resolve(x.data))
+                .catch(x => {
+                    alert(x); // handle error
+                    reject(x);
+                });
+        });
+    }
+
     getOrders() {
         return new Promise((resolve, reject) => {
             axios.get(`${this.url}/manufacturerorders`)
