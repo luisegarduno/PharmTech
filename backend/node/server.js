678a7b6cf67834a8de217f94fdf4a91203fbca7c
luisegarduno <luis.e.garduno@gmail.com>
2020-04-25 23:16:55 -0500

    Added /pharmacyNotification

--- backend/node/server.js
+++ backend/node/server.js
@@ -114,6 +114,8 @@ app.get('/getDrugTypes', (req, res) => {
   });
 });
 
+
+
 //gets inventory information for doctor
 app.get('/getDoctorInventory', (req, res) => {
   connection.query('SELECT batch_id, d.name, i.drug_id, quantity, exp_date, t.name AS drug_type, r.related FROM inventory AS i LEFT JOIN drugs AS d ON d.id = i.drug_id LEFT JOIN (SELECT drug_type, GROUP_CONCAT(DISTINCT name) AS related FROM drugs GROUP BY drug_type) AS r ON d.drug_type = r.drug_type JOIN drug_types AS t ON t.id = d.drug_type;', function (err, rows, fields) {
@@ -154,6 +156,24 @@ app.get('/getInventory/:id', (req, res) => {
 //inventory for pharmacist and manager
 app.get('/pharmacyInventory', (req, res) => {
   connection.query('SELECT d.name, d.id, i.quantity, i.exp_date FROM inventory i join drugs d on i.drug_id = d.id', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+//inventory for pharmacist and manager
+app.get('/pharmacyNotification', (req, res) => {
+  connection.query('SELECT d.name, d.id AS DrugID,CASE WHEN SUM(i.quantity) <= 0 OR SUM(i.quantity) IS NULL THEN "OutOfStock" WHEN SUM(i.quantity) > 0 THEN "InStock" ELSE "Error" END AS Available FROM inventory i JOIN drugs d ON i.drug_id = d.id GROUP BY d.id', function (err, rows, fields) {
     if (err) {
       logger.error("Error while executing Query");
       res.status(400).json({
