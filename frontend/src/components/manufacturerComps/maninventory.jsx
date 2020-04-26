731cb79aa309b7f219bd6f248a3125947533ea89
Adam <aczarnik@smu.edu>
2020-04-25 23:52:18 -0700

    working on API functions

--- frontend/src/components/manufacturerComps/maninventory.jsx
+++ frontend/src/components/manufacturerComps/maninventory.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../../images/erpharmtechgrayer.png";
 import {Link} from "react-router-dom";
 import { ManufacturerRepository } from "../../API";
+import _ from 'lodash';
 
 export class Maninventory extends React.Component {
 
@@ -14,6 +15,7 @@ export class Maninventory extends React.Component {
         this.username = localStorage['username']
         this.state = {
             drugs:[],
+            sortDirection : 'asc',
         }
         this.truefalse = this.truefalse.bind(this);
         this.yesno = this.yesno.bind(this);
@@ -25,6 +27,21 @@ export class Maninventory extends React.Component {
 
     componentDidMount() {
         this.manufacturerRepository.getInventory().then(Drug => this.setState({drugs : Drug.data}))
+    }
+
+    sortBy(field) {      
+        if (this.state.sortDirection == 'asc') {
+            this.setState({sortDirection: 'desc'})
+            this.setState({ 
+                drugs: _.orderBy(this.state.drugs, field, this.state.sortDirection)
+            });
+        }
+        if (this.state.sortDirection == 'desc') {
+            this.setState({sortDirection: 'asc'})
+            this.setState({ 
+                drugs: _.orderBy(this.state.drugs, field, this.state.sortDirection)
+            });
+        }
     }
 
     truefalse(value) {
@@ -89,24 +106,24 @@ export class Maninventory extends React.Component {
             <h1 className = "tableHeader">All Inventory</h1>
                 <div className = "itemsTable">
                     <table>
-                        <tr class="">
-                            <th>Name</th>
-                            <th>Made On</th>
-                            <th>Acq From</th>
-                            <th>Expired?</th>
-                            <th>OK to Sell?</th>
-                        </tr>
+                        <thead><tr className="">
+                            <th><button type="button" id="expDate" onClick={this.sortBy.bind(this, 'name')}>Name</button></th>
+                            <th><button type="button" id="expDate" onClick={this.sortBy.bind(this, 'exp_date')}>Made On</button></th>
+                            <th><button type="button" id="expDate" onClick={this.sortBy.bind(this, 'aquired_from')}>Acquired From</button></th>
+                            <th><button type="button" id="expDate" onClick={this.sortBy.bind(this, 'expired')}>Expired?</button></th>
+                            <th><button type="button" id="expDate" onClick={this.sortBy.bind(this, 'ok_to_sell')}>OK to Sell?</button></th>
+                        </tr></thead>
+                        <tbody>
                         {this.state.drugs.map(item => (
-                            <tr>
+                            <tr key={item.name}>
                                 <td id="item">{item.name}</td>
                                 <td id="item">{this.formatDate(item.exp_date)}</td>
                                 <td id="item">{item.aquired_from}</td>
-                                {/* <td id="item">{item.expired}<form><button onClick = {this.toggleExpired(item)}>Flip Expired</button></form></td> */}
-                                {/* <td id="item">{item.ok_to_sell}<form><button onClick = {this.toggleSell(item)}>Flip Ok to Sell</button></form></td> */}
                                 <td id="item">{this.yesno(item.expired)} <button className="btn btn-primary">Mark {this.opposite(item.expired)}</button></td>
                                 <td id="item">{this.yesno(item.ok_to_sell)} <button className="btn btn-primary" onClick={this.toggleSell(item.ok_to_sell, item.batch_id)}>Mark {this.opposite(item.ok_to_sell)}</button></td>
                             </tr>
                         ))}
+                        </tbody>
                     </table>
                 </div>
                 <Link to="/manufacturer">
