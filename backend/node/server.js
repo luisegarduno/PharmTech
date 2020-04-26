a620bc85a7dc2b58e646d1b8d240221e9ca52ec0
Manqi Shi <Manqi@Manqis-Air-3.attlocal.net>
2020-04-26 18:09:49 -0500

    added get user

--- backend/node/server.js
+++ backend/node/server.js
@@ -45,6 +45,24 @@ app.get('/', (req, res) => {
   res.status(200).send('Go to 0.0.0.0:3000.');
 });
 
+//get list of users
+app.get('/getUser', (req, res) => {
+  connection.query('SELECT * FROM user', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
 //get user
 app.post('/verifyUser', (req, res) => {
   connection.query('SELECT IF(EXISTS(SELECT * FROM user WHERE username = ? AND hashpass = ? AND userType_id = ?), (SELECT first_name AS result FROM user WHERE hashpass = ?), 0) AS result;', [req.body.username, req.body.password, req.body.type, req.body.password], function (err, rows, fields) {
@@ -61,6 +79,8 @@ app.post('/verifyUser', (req, res) => {
   });
 });
 
+//POST
+//add in user
 app.post('/registerUser', (req, res) => {
   connection.query('INSERT INTO user (first_name, last_name, username, hashpass, email, userType_id) VALUES ();', [req.body.firstname, req.body.lastname, req.body.username, req.body.password, req.body.email, req.body.type], function (err, rows, fields) {
     if (err) {
