692fd6be20a63f89fb5d1f52e6e8fd7cd9906b31
Adam <aczarnik@smu.edu>
2020-04-25 19:34:59 -0700

    working on true/false values

--- frontend/src/components/manufacturerComps/maninventory.jsx
+++ frontend/src/components/manufacturerComps/maninventory.jsx
@@ -15,36 +15,64 @@ export class Maninventory extends React.Component {
         this.state = {
             drugs:[],
         }
+        this.truefalse = this.truefalse.bind(this);
+        this.yesno = this.yesno.bind(this);
+        this.opposite = this.opposite.bind(this);
+        this.toggleExpired = this.toggleExpired.bind(this);
+        this.toggleSell = this.toggleSell.bind(this);
         this.formatDate = this.formatDate.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.manufacturerRepository.getInventory().then(Drug => this.setState({drugs : Drug.data}))
     }
 
-    toggleExpired(item) {
-        var tf = false; //0 = true
-        if (item.expired == false) {
-            tf = true;
+    truefalse(value) {
+        if(value == true) {
+            return "True";
+        } else {
+            return "False";
         }
-        var batchid = item.id;
-        this.manufacturerRepository.markExpired(tf, batchid);
-        this.manufacturerRepository.getInventory().then(Drug => this.setState({drugs : Drug.data}));
     }
 
-    toggleSell(item) {
+    yesno(value) {
+        if (value == true) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+
+    opposite(value) {
+        if (value == true) {
+            return "No";
+        } else {
+            return "Yes";
+        }
+    }
+
+    toggleExpired(item) {
+        var tf = 0; //0 = true
+        if (item.expired == false) {
+            tf = 1;
+        }
+        var batchid = item.batch_id;
+        this.manufacturerRepository.markExpired(tf, batchid);
+        // this.manufacturerRepository.getInventory().then(Drug => this.setState({drugs : Drug.data}));
+    }
+
+    toggleSell(oksell, batchid) {
         var tf = false; //0 = true
-        if (item.ok_to_sell == false) {
+        if (oksell == false) {
             tf = true;
         }
-        var batchid = item.id;
         this.manufacturerRepository.markBuy(tf, batchid);
-        this.manufacturerRepository.getInventory().then(Drug => this.setState({drugs : Drug.data}));
+        // this.manufacturerRepository.getInventory().then(Drug => this.setState({drugs : Drug.data}));
     }
 
     formatDate(myDate){
-        // var d = myDate.getMonth() + "-" + myDate.getDate() + "-" + myDate.getFullYear();
-        return myDate;
+        var d = myDate.substring(5,7) + "-" + myDate.substring(8,10) + "-" + myDate.substring(0,4);
+        return d;
     }
 
     render() {
@@ -61,7 +89,7 @@ export class Maninventory extends React.Component {
             <h1 className = "tableHeader">All Inventory</h1>
                 <div className = "itemsTable">
                     <table>
-                        <tr class="headerFixed">
+                        <tr class="">
                             <th>Name</th>
                             <th>Made On</th>
                             <th>Acq From</th>
@@ -72,9 +100,11 @@ export class Maninventory extends React.Component {
                             <tr>
                                 <td id="item">{item.name}</td>
                                 <td id="item">{this.formatDate(item.exp_date)}</td>
-                                <td id="item">{item.acquired_from}</td>
-                                <td id="item">{item.expired}<form><button onClick = {this.toggleExpired(item)}>Flip Expired</button></form></td>
-                                <td id="item">{item.ok_to_sell}<form><button onClick = {this.toggleSell(item)}>Flip Ok to Sell</button></form></td>
+                                <td id="item">{item.aquired_from}</td>
+                                {/* <td id="item">{item.expired}<form><button onClick = {this.toggleExpired(item)}>Flip Expired</button></form></td> */}
+                                {/* <td id="item">{item.ok_to_sell}<form><button onClick = {this.toggleSell(item)}>Flip Ok to Sell</button></form></td> */}
+                                <td id="item">{this.yesno(item.expired)} <button className="btn btn-primary">Mark {this.opposite(item.expired)}</button></td>
+                                <td id="item">{this.yesno(item.ok_to_sell)} <button className="btn btn-primary" onClick={this.toggleSell(item.ok_to_sell, item.batch_id)}>Mark {this.opposite(item.ok_to_sell)}</button></td>
                             </tr>
                         ))}
                     </table>
