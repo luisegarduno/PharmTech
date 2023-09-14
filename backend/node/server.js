8f13f469a7ec683edee3508c36b0bc2d8950bc34
luisegarduno <luis.e.garduno@gmail.com>
2023-09-14 19:21:57 -0400

    - Revert changes in server.js

--- backend/node/server.js
+++ backend/node/server.js
@@ -26,20 +26,20 @@ app.use(cors({
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 // const user = require('./routes/user')
-const sales = require('./routes/sales')
+// const sales = require('./routes/sales')
 // const orders = require('./routes/orders')
 // const inventory = require('./routes/inventory')
 // const prescription = require('./routes/prescription')
-const notification = require('./routes/notification')
-const manufacturer = require('./routes/manufacturer')
-
+// const notification = require('./routes/notification')
+// const manufacturer = require('./routes/manufacturer')
+// 
 // user(app, logger);
-sales(app, logger);
+// sales(app, logger);
 // orders(app, logger);
 // inventory(app, logger);
 // prescription(app, logger);
-notification(app, logger);
-manufacturer(app, logger);
+// notification(app, logger);
+// manufacturer(app, logger);
 
 app.get('/', (req, res) => {
   res.status(200).send('Go to 0.0.0.0:3000.');
@@ -218,6 +218,76 @@ app.get('/pharmacyNotification', async (req, res) => {
   });
 });
 
+// GET
+// Returns all notifications for given username
+app.get('/userNotifications/:id', async (req, res) => {
+  mysqlConnect.query('SELECT n.id AS NotificationID, d.id AS DrugID, d.name AS DrugName, n.drugs_status AS InventoryStatus FROM notifications n JOIN drugs d ON d.id = drug_id JOIN user u ON u.id = n.pharmacist_id JOIN user_type ut ON u.userType_id = ut.id WHERE ut.type IN ("pharmacist", "pharmacy manager") AND u.first_name = ?;', [req.params.id], function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+// GET
+// Scans entire inventory and returns value on whether given drugID is InStock or OutOfStock
+app.get('/pharmacyNotification/:id', async (req, res) => {
+  mysqlConnect.query('SELECT d.name, d.id AS DrugID,CASE WHEN SUM(i.quantity) <= 0 OR SUM(i.quantity) IS NULL THEN "Out Of Stock" WHEN SUM(i.quantity) > 0 THEN "In Stock" ELSE "Error" END AS Available FROM inventory i JOIN drugs d ON i.drug_id = d.id WHERE d.id = ? GROUP BY d.id', [req.params.id], function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+
+// POST
+// Post Notification 
+app.post('/addNotification/:id', async (req, res) => {
+
+  var drugID = req.body.drug_id;
+
+  mysqlConnect.query("INSERT INTO `pharmtech`.`notifications`(pharmacist_id, drug_id, drugs_status)(SELECT u.id, ?, (SELECT CASE WHEN SUM(i.quantity) <= 0 OR SUM(i.quantity) IS NULL THEN 'Out Of Stock' WHEN SUM(i.quantity) > 0 THEN 'In Stock' ELSE 'Error' END AS drugs_status FROM inventory i WHERE i.drug_id = n.drug_id GROUP BY i.drug_id) FROM `pharmtech`.`notifications` AS n INNER JOIN user u ON u.first_name = ? INNER JOIN user_type ut ON u.userType_id = ut.id WHERE ut.type IN ('pharmacist', 'pharmacy manager') LIMIT 1)", [drugID, req.params.id], function (err, rows, fields) {
+    if (err){
+      logger.error("Problem inserting into prescription table");
+    }
+    else {
+      res.status(200).send(`added to the table!`);
+    }
+  });
+});
+
+
+
+// DELETE
+// Removes notification/s with drug_id for username "/:id" from notification table 
+app.delete('/deleteNotification/:id', async (req, res) => {
+  var drugID = req.query.drug_id;
+
+  mysqlConnect.query("DELETE n FROM `pharmtech`.`notifications` AS n INNER JOIN user u ON u.id = n.pharmacist_id INNER JOIN user_type ut ON u.userType_id = ut.id WHERE n.`drug_id` = ? AND ut.type IN ('pharmacist', 'pharmacy manager') AND u.first_name = ?", [drugID, req.params.id], function (err, result, fields) {
+    if (err) 
+      return console.error(error.message);
+    res.end(JSON.stringify(result)); 
+    });
+});
+
+
 // GET : drug id
 // Returns infomation for specific drugID
 app.get('/pharmacyInventory/:id', (req, res) => {
@@ -243,6 +313,118 @@ app.get('/pharmacyInventory/:id', (req, res) => {
 app.get('/getPrescription/:id', (req, res) => {
  
   mysqlConnect.query('SELECT p.id as prescription_id, CONCAT(u.first_name, " ", u.last_name) as patient_name, d.name, p.quantity, p.fill_date, p.create_date, CONCAT(u2.first_name, " ", u2.last_name) AS doctor_name FROM `pharmtech`.`prescriptions` p join user u on u.id = p.patient_id join user u2 on u2.id = p.doctor_id join `pharmtech`.`drugs` d on d.id = p.drug_id WHERE p.id = ?', [req.params.id], function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+//pharmacy revenues
+app.get('/getPhamRequest', (req, res) => {
+
+  mysqlConnect.query('SELECT o.id, o.drug_id, d.name, o.quantity, d.unit_measure, o.date_requested FROM `pharmtech`.`order_requests` o join drugs d on d.id = o.drug_id', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+
+//pharmacy revenues
+app.get('/getRevenues', (req, res) => {
+  mysqlConnect.query('SELECT d.name, d.sell_price * p.quantity FROM `pharmtech`.`prescriptions` p join `pharmtech`.`drugs` d on d.id = p.drug_id WHERE p.fill_date IS NOT NULL', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+//pharmacy expenses
+app.get('/getExpenses', (req, res) => {
+  mysqlConnect.query('SELECT d.name, d.purchase_price * io.quantity FROM `pharmtech`.`inventory_orders` io join `pharmtech`.`drugs` d on d.id = io.drug_id WHERE io.fulfill_date IS NOT NULL', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+
+
+//pharmacist manager sales pages
+app.get('/getPharmManagerSales', (req, res) => {
+  mysqlConnect.query('SELECT d.name, p.quantity, d.sell_price FROM `pharmtech`.`prescriptions` p join `pharmtech`.`drugs` d on d.id = p.drug_id WHERE p.fill_date IS NOT NULL', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+//pharmacist manager recent sales pages
+app.get('/getRecentPharmManagerSales', (req, res) => {
+  mysqlConnect.query('SELECT d.name, p.quantity, d.sell_price FROM `pharmtech`.`prescriptions` p join `pharmtech`.`drugs` d on d.id = p.drug_id WHERE p.fill_date IS NOT NULL ORDER BY p.fill_date DESC LIMIT 5', function (err, rows, fields) {
+    if (err) {
+      logger.error("Error while executing Query");
+      res.status(400).json({
+        "data": [],
+        "error": "MySQL error"
+      })
+    }
+    else{
+      res.status(200).json({
+        "data": rows
+      });
+    }
+  });
+});
+
+//pharmacy sales
+app.get('/getSales', (req, res) => {
+  mysqlConnect.query('SELECT u.first_name, u.last_name, d.name, d.sell_price FROM `pharmtech`.`prescriptions` p join `pharmtech`.`drugs` d on d.id = p.drug_id join `pharmtech`.`user` u on u.id = p.patient_id WHERE p.fill_date IS NOT NULL LIMIT 5', function (err, rows, fields) {
     if (err) {
       logger.error("Error while executing Query");
       res.status(400).json({
